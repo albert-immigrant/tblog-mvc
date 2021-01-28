0af206a936fded16e2901178b59969c042592694
Gitea <gitea@fake.local>
2021-01-28 12:30:29 +0800

    update some metnods

---
+++ src/main/java/cz/jiripinkas/jba/controller/IndexController.java
@@ -0,0 +1,39 @@
+package cz.jiripinkas.jba.controller;
+
+import cz.jiripinkas.jba.entity.Topic;
+import cz.jiripinkas.jba.repository.TopicRepository2;
+import cz.jiripinkas.jba.service.ItemService;
+import cz.jiripinkas.jba.service.TopicService2;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.Optional;
+
+@Controller
+public class IndexController {
+	
+	@Autowired
+	private ItemService itemService;
+
+    @Autowired
+	private TopicService2 topicService2;
+    Topic topic;
+
+	String strReasons;
+	@RequestMapping("/index")
+	public String index(Model model) {
+
+		try {
+			topic=topicService2.findTopicById("1");
+		}
+		catch (Exception e){strReasons=e.getMessage();}
+		finally {
+
+		}
+
+		model.addAttribute("items", itemService.getItems());
+		return "index";
+	}
+}
---
+++ src/main/java/cz/jiripinkas/jba/service/TopicService2.java
@@ -0,0 +1,38 @@
+package cz.jiripinkas.jba.service;
+
+import cz.jiripinkas.jba.entity.Category;
+import cz.jiripinkas.jba.entity.Topic;
+import cz.jiripinkas.jba.vo.IndexVo;
+import cz.jiripinkas.jba.vo.TopicVo;
+import org.springframework.data.domain.Page;
+
+import java.util.List;
+
+public interface TopicService2 {
+
+    Topic addTopic(TopicVo vo);
+    Topic addTopic(TopicVo vo,Category category);
+
+    Topic findTopicById(String topicId);
+
+    Page<Topic> findByPage(int pageNo, int pageSize);
+
+    Page<Topic> findByUsernameAndPage(String username, int pageNO, int pageSize);
+
+    Object findTopicVoById(String topicID);
+
+    Topic updateById(TopicVo vo, String topicID);
+
+    void deleteById(String topicID);
+
+    void increaseReplyCount(String topicid);
+
+    void decreaseReplyCount(String topicId);
+
+
+    Object findByPage(IndexVo vo);
+
+    void increaseVisitCount(String topicID);
+
+     TopicVo convertToVo(Topic topic);
+}
